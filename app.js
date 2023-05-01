19237c94a7e7c42d0371a6867515b2c995c18a4d
Christ_YAO <christyao1299@gmail.com>
2023-05-01 07:29:09 +0100

    commit
    commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const btnEntrant = document.querySelector('.btn1');
+const btnSortant = document.querySelector('.btn2');
+const textEntrant = document.querySelector('.entrant1');
+const textSortant = document.querySelector('.sortant1');
+const tabEntrant = document.querySelector('.entrant');
+const tabSortant = document.querySelector('.sortant');
+
+
+btnEntrant.addEventListener('click', () => {
+    console.log(btnEntrant)
+        tabEntrant.style.display="flex";
+        tabSortant.style.display="none";
+        textEntrant.style.display="flex";
+        textSortant.style.display="none";
+        btnEntrant.classList.add('active');
+        btnSortant.classList.remove('active');
+    })
+
+btnSortant.addEventListener('click', () => {
+    console.log(btnSortant)
+        textEntrant.style.display="none";
+        textSortant.style.display="flex";
+        tabEntrant.style.display="none";
+        tabSortant.style.display="flex";
+        btnEntrant.classList.remove('active');
+        btnSortant.classList.add('active');
+    })
+
+
+
